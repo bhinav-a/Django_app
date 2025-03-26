e9c2392fb85643a4ae6648ef9ace78154b7410a2
bhinav-a <anuragabhinav08@gmail.com>
2025-03-26 16:53:08 +0530

    seed.py

--- core/vege/models.py
+++ core/vege/models.py
@@ -8,3 +8,36 @@ class Receipe(models.Model):
     r_name = models.CharField(max_length=255)
     r_description = models.TextField()
     r_image = models.ImageField(upload_to="images") 
+
+class Department(models.Model):
+    department = models.CharField(max_length=100)
+
+    def __str__(self) -> str :
+        return self.department
+    
+    class Meta :
+        ordering = ['department']
+
+class StudentId(models.Model):
+    student_id = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.student_id
+    
+
+class Student(models.Model):
+    
+    S_Department = models.ForeignKey(Department , related_name="depart" , on_delete=models.CASCADE)
+    S_Id = models.OneToOneField(StudentId , on_delete=models.CASCADE , related_name="studentid")
+    S_name = models.CharField(max_length=100)
+    S_email = models.EmailField(unique=True)
+    S_phone = models.CharField(default=18)
+    S_address = models.TextField()   
+
+    def __str__(self) -> str:
+        return self.student_name
+
+    class Meta :
+        ordering  = ['student_name']
+        verbose_name = "student"
+         
